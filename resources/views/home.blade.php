01482af1cb049f45e96a6ecf89ecae65a661a277
battistaennio <167311318+battistaennio@users.noreply.github.com>
2024-09-12 15:21:44 +0200

    Initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.main')
+
+@section('content')
+    <div class="container my-5">
+        <h1>Home</h1>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime ex, ea dolor repellat quibusdam distinctio,
+            provident facilis deleniti ab aliquid error soluta maiores aspernatur unde nisi dolorum ducimus et perferendis!
+        </p>
+    </div>
+@endsection
+
+
+@section('titlePage')
+    home
+@endsection
---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,3 @@
+<footer class="my-5 text-center">
+    <p>by 128</p>
+</footer>
